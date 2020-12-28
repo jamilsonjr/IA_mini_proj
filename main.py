7b90adf876808e75379a6491891d0aaed422b971
jamilsonjr <jamilsonpinheiro.jr@gmail.com>
2020-12-28 16:02:28 +0000

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+import probability from 'aima-python'
+class MDProblem :
+    def __init__ ( self , fh ) :
+    # Place here your code to load problem from opened file object fh
+    # and use probability . BayesNet () to create the Bayesian network .
+    def solve ( self ) :
+    # Place here your code to determine the maximum likelihood
+    # solution returning the solution disease name and likelihood .
+    # Use probability . elimination_ask () to perform probabilistic
+    # inference .
+    return ( disease , likelihood )
